ae0864a221ea09dfa4477acd19f3b824b4d350f3
Vladimir <zakharov.2004@yandex.ru>
2024-12-03 12:14:18 +0300

    baseline link prediction

--- scripts/gat.py
+++ scripts/gat.py
@@ -3,13 +3,17 @@ from dgl.nn import SAGEConv
 import torch.nn.functional as F
 
 class GraphSAGE(nn.Module):
-    def __init__(self, in_feats, h_feats):
+    def __init__(self, in_feats, h_feats, dropout=False):
         super(GraphSAGE, self).__init__()
         self.conv1 = SAGEConv(in_feats, h_feats, 'mean')
+        if dropout:
+            self.dropout = nn.Dropout(p=0.2)
         self.conv2 = SAGEConv(h_feats, h_feats, 'mean')
     
     def forward(self, g, in_feat):
         h = self.conv1(g, in_feat)
+        if hasattr(self, 'dropout'):
+            h = self.dropout(h)
         h = F.relu(h)
         h = self.conv2(g, h)
         return h
